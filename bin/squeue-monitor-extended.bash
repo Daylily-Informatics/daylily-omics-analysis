54971f06e2c9c7647d902ccf41d9f1e1594e9635
AWS ParallelCluster user <ubuntu@ip-10-0-0-214.us-west-2.compute.internal>
2026-02-16 11:07:19 +0000

    X

--- bin/squeue-monitor-extended.bash
+++ bin/squeue-monitor-extended.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-watch -n 2 squeue -o \"%.8i %.10P %.24j %.8u %.2t %.10M %.4C %.10m %.6Q %.8p %R %Z\"
+watch -n 2 squeue -o \"%.8i %.10P %.29j %.8u %.2t %.10M %.4C %.10m %.6Q %.8p %R %Z\"
