bc360cab79eb8226a0914ac41f9a3c71f4a13dee
AWS ParallelCluster user <ubuntu@ip-10-0-0-214.us-west-2.compute.internal>
2026-02-18 11:02:28 +0000

    X

--- bin/augment_setup_and_run_dayoa.bash
+++ bin/augment_setup_and_run_dayoa.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-$GENOME_CODE=$2
-$LOCAL_OR_SLURM=$1
-$SNAKEMAKE_TARGETS=$3 # " produce_snv_concordances produce_alignstats produce_sentdhio_vcf "
-$SNAKEMAKE_FLAGS=$4 # " -p -j 10 -k -T 1 "
-$DRY_RUN=$5 # " -n "
+GENOME_CODE=$2
+LOCAL_OR_SLURM=$1
+SNAKEMAKE_TARGETS=$3 # " produce_snv_concordances produce_alignstats produce_sentdhio_vcf "
+SNAKEMAKE_FLAGS=$4 # " -p -j 10 -k -T 1 "
+DRY_RUN=$5 # " -n "
 
-source dyoainit && source bin/day_activate "$LOCAL_OR_SLURM" "$GENOME_CODE" && bin/day_run "$SNAKEMAKE_TARGETS" "$SNAKEMAKE_FLAGS" "$DRY_RUN"
+
+
+
+source dyoainit
+source bin/day_activate "$LOCAL_OR_SLURM" "$GENOME_CODE"
+bin/day_run "$SNAKEMAKE_TARGETS" "$SNAKEMAKE_FLAGS" "$DRY_RUN"
