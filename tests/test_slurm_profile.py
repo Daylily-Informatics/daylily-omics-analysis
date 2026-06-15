375e5f796be792de15287a646a39d0960c37aa8a
John Major <iamh2o@gmail.com>
2026-06-14 21:57:07 -0700

    Raise Slurm default memory

--- tests/test_slurm_profile.py
+++ tests/test_slurm_profile.py
@@ -109,6 +109,7 @@ def test_slurm_profile_default_partition_includes_384_vcpu_queue() -> None:
     )
 
     assert "partition=i384nvme,i192,i192nvme,i128" in profile["default-resources"]
+    assert "mem_mb=100000" in profile["default-resources"]
 
 
 def test_slurm_rule_config_uses_v8_partitions_and_explicit_memory() -> None:
