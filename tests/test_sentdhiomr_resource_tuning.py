9c2d540cd968bf45207bd8acd5a26d61048e750f
John Major <iamh2o@gmail.com>
2026-07-07 13:38:10 -0700

    Move sentdhiomr transfer scratch to FSx

--- tests/test_sentdhiomr_resource_tuning.py
+++ tests/test_sentdhiomr_resource_tuning.py
@@ -44,7 +44,7 @@ def test_slurm_sentdhiomr_conservative_tuning_values() -> None:
     assert sentdhiomr["mem_mb"] == 300000
     assert sentdhiomr["sr_align_tmp_parent"] == "/scratch"
     assert sentdhiomr["stage3_tmp_parent"] == "/fsx/scratch"
-    assert sentdhiomr["transfer_tmp_parent"] == "/tmp"
+    assert sentdhiomr["transfer_tmp_parent"] == "/fsx/scratch"
 
 
 def test_local_sentdhiomr_declares_tuning_keys_for_parseability() -> None:
